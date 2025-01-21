d3e89ca4a8d83483259bb71a9336a08ce7d32c15
Serim Son <serim711@gmail.com>
2025-01-21 22:44:33 +0900

    add navigation compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.bundles.ktor.client)
     implementation(libs.kotlinx.datetime)
@@ -83,5 +84,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    androidTestImplementation(libs.androidx.navigation.testing)
 
 }
